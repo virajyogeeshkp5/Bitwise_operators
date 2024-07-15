cef9998ed10ca79d757cb0a78ca59fbb8812e969
YOGEESH K P <yogeesh793@gmail.com>
2024-07-15 09:41:05 +0530

    Add files via upload

---
+++ Bitwise_operators.py
@@ -0,0 +1,22 @@
+# Bitwise operators
+a=10
+b=4
+print("Bitwise operator")
+# Binary AND
+c = a&b
+print("a&b:",c)
+# Binary OR
+c = a|b
+print("a|b:",c)
+# Binary XOR
+c=a^b
+print("a^b:",c)
+# Binary ones Compliment
+c=~a
+print("~a:",c)
+# Binary Left shift
+c=a<<2
+print("a<<2:",c)
+# Binary Right shift
+c=a>>2
+print("a>>2:",c)
